0a4bac76baffb0cebf9706d0cccbb60a3d97b9df
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-05 19:29:29 +0100

    Create the navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
